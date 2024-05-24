4f127c14f835f169da833cf38362986a1d6e9771
DECIO SANTANA DE AGUIAR <decioagu@gmail.com>
2024-05-24 10:50:08 -0300

    24/05/2024

---
+++ alembic/versions/ce366017772f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ce366017772f
+Revises: c006e8463eb4
+Create Date: 2024-05-23 11:01:11.687245
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ce366017772f'
+down_revision = 'c006e8463eb4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
